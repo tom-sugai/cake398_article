2238c9909be0ba562d6eed09109cb9e32dcc2cb9
tom-sugai <tom.sugai@gmail.com>
2023-02-24 11:10:26 +0900

    change timezone to 'UTC' from 'Asia/Tokyo'

--- config/app.php
+++ config/app.php
@@ -282,8 +282,8 @@ return [
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
             //'encoding' => 'utf8mb4',
-            //'timezone' => 'UTC',
-            'timezone' => 'Asia/Tokyo',
+            'timezone' => 'UTC',
+            //'timezone' => 'Asia/Tokyo',
             'flags' => [],
             'cacheMetadata' => true,
             'log' => false,
